4953624bdd8fc4a3ed623bc6963f8e34cb81a8f4
MuhammadRaheelAmir <raheelamir0911@gmail.com>
2025-12-07 19:46:15 +0500

    Adding the Operators file

---
+++ 1_r_basics/3_operators.R
@@ -0,0 +1,25 @@
+# R Operators
+
+a <- 15
+b <- 4
+
+# Arithmetic operators
+print(a + b)   # add
+print(a - b)   # subtract
+print(a * b)   # multiply
+print(a / b)   # divide
+print(a %% b)  # remainder
+print(a ^ b)   # power
+
+# Comparison operators
+print(a > b)   # greater
+print(a < b)   # less
+print(a == b)  # equal
+print(a != b)  # not equal
+
+# Logical operators
+x <- TRUE
+y <- FALSE
+print(x & y)   # AND
+print(x | y)   # OR
+print(!x)      # NOT
